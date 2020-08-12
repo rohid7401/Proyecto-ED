38461cd24e7e292e8c265cb5e85bff22b96b2b09
Maria Laura Sánchez H <lalysanchez2001@egmail.com>
2020-08-12 16:46:54 -0600

    Prim

---
+++ londres-master/Prim.cpp
@@ -0,0 +1,42 @@
+#include "Prim.h"
+#include <priority_queue>
+
+typedef pair<double, int> di;
+typedef pair<int, int> ii;
+
+vector<pair<int, int> > prim(Grafo& grafo)
+{
+
+	vector<pair <int, int> > res; 
+	vector<int> padre(grafo.NumeroVertices, -1);
+	priority_queue < pair <double, int>, vector< pair <double, int> >, greater < pair <double, int> > > q;
+	vector<bool> seen(grafo.NumeroVertices, false);
+	q.push(di(0, 0));
+	int restantes = grafo.NumeroVertices - 1;
+
+	while (restantes) {
+	
+		di actual = q.top(); q.pop();
+		int nodoActual = actual.second;
+
+		if (seen[nodoActual]) {
+			continue;
+		}
+
+		res.push_back(ii(nodoActual, padre[nodoActual]));
+		seen[nodoActual] = true;
+		restantes--;
+
+		for (auto i : grafo.Vertices[nodoActual].Vecinos) {
+		
+			int vecino = i.VerticeAdyacente;
+
+			if (!seen[vecino]) {
+			
+				q.push(di(i.Tiempo, vecino));
+
+			}
+		}
+	}
+	return res;
+}
